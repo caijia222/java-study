713a93bd931adc054fe4f55870a731a208f98865
caijia222 <caijia222@gmail.com>
2021-03-15 22:48:50 +0800

    exception

--- src/exception/ExceptionTest.java
+++ src/exception/ExceptionTest.java
@@ -1,9 +1,11 @@
 package exception;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Optional;
 
 import org.junit.Test;
 
@@ -38,5 +40,93 @@ public class ExceptionTest {
 			return ++x;
 		}
 	}
+	
+	@Test
+	public void test3() {
+		try {
+			process1();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void process1() {
+		try {
+			process2();
+		} catch (NullPointerException e) {
+			IllegalArgumentException le = new IllegalArgumentException(e);
+			System.out.println("获取子异常="+le.getCause());
+			throw le;
+		}
+	}
+	
+	static void process2() {
+		throw new NullPointerException();
+	}
+	
+	@Test
+	public void test4() {
+		try {
+			Integer.parseInt("abd");
+		} catch (Exception e) {
+			System.out.println("catch");
+			throw new RuntimeException(e);
+		}finally {
+			System.out.println("finally");
+		}
+	}
+	
+	@SuppressWarnings("finally")
+	@Test
+	public void test5() throws Exception {
+		Exception origin = null;
+		try {
+			Integer.parseInt("abc");
+		} catch (Exception e) {
+			origin = e;
+			throw e;
+		}finally {
+			Exception e = new IllegalArgumentException();
+			if(origin != null) e.addSuppressed(origin);
+			throw e;
+		}
+	}
+	
+	@Test
+	public void test6() {
+		Optional<String> readFromFile = readFromFile("accc");
+		System.out.println(readFromFile.isPresent());
+	}
 
+	static Optional<String> readFromFile(String file){
+		if(!Files.exists(Paths.get("file"))) {
+			return Optional.empty();
+		}
+		return Optional.of("result");
+	}
+}
+
+class BaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1411869357571964819L;
+	public BaseException() {
+		super();
+	}
+	public BaseException(Throwable e) {
+		super(e);
+	}
+	public BaseException(String message) {
+		super(message);
+	}
+	public BaseException(String message, Throwable e) {
+		super(message,e);
+	}
+}
+
+class LoginFailedException extends BaseException{
+
+	private static final long serialVersionUID = 3830721778119749535L;
+	public LoginFailedException(String message) {
+		super(message);
+	}
 }
