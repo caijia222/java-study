a4214b281a6238adb0fb5772d8acaed3b27042c5
caijia222 <caijia222@gmail.com>
2021-03-16 22:41:54 +0800

    log

--- src/log/LogTest.java
+++ src/log/LogTest.java
@@ -1,21 +1,22 @@
 package log;
 
-import java.util.logging.Logger;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LogTest {
 
 	@Test
 	public void test() {
-		Logger logger = Logger.getGlobal();
+		java.util.logging.Logger logger = java.util.logging.Logger.getGlobal();
         logger.info("start process...");
         logger.warning("memory is running out...");
         logger.fine("ignored.");
         logger.severe("process will be terminated...");
-        logger = Logger.getLogger(LogTest.class.getName());
+        logger = java.util.logging.Logger.getLogger(LogTest.class.getName());
         logger.severe("有一个严重问题");
         
 	}
@@ -26,5 +27,13 @@ public class LogTest {
 		log.info("start...");
 		log.warn("end.");
 		log.error("错误");
+	}
+	
+	@Test
+	public void test3() {
+		Logger logger = LoggerFactory.getLogger(LogTest.class);
+		logger.info("start...{}",LogTest.class.getName());
+		logger.warn("end.");
+		logger.error("错误");
 	}
 }
